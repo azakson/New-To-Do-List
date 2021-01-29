ff4358ae54cd0084bc8a455b528ed4f78ba816ae
Avery Zakson <avery.zakson@gmail.com>
2021-01-29 10:35:17 -0600

    Initial Commit

---
+++ to do/to_doApp.swift
@@ -0,0 +1,17 @@
+//
+//  to_doApp.swift
+//  to do
+//
+//  Created by Avery Zakson on 1/29/21.
+//
+
+import SwiftUI
+
+@main
+struct to_doApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
