536c0fe6bccd34c3826dbdf87272d5d1123f34ea
Avery Zakson <avery.zakson@gmail.com>
2021-01-29 10:49:34 -0600

    Created new project for new version of Xcode. Added edit and delete functions to app.

--- to do/ContentView.swift
+++ to do/ContentView.swift
@@ -1,16 +1,30 @@
 //
 //  ContentView.swift
-//  to do
+//  To Do List
 //
-//  Created by Avery Zakson on 1/29/21.
+//  Created by Avery Zakson on 1/28/21.
+//  Copyright © 2021 Avery Zakson. All rights reserved.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State var things = ["First Thing", "Second Thing", "Third Thing", "Fourth Thing", "Fifth Thing"]
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(things, id: \.self) { thing in Text(thing)
+                }
+                .onMove(perform: { indices, newOffset in
+                    things.move(fromOffsets: indices, toOffset: newOffset)
+                })
+                .onDelete(perform: { indexSet in
+                    things.remove(atOffsets: indexSet)
+                })
+            }
+            .navigationBarTitle("Things")
+            .navigationBarItems(leading: EditButton())
+        }
     }
 }
 
@@ -19,3 +33,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
